e2e39eb6023b556e10d458a2947b27aff997cacc
Nnley <kragelek86@gmail.com>
2024-10-19 08:53:05 +0300

    spotify auth & bot

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import logging
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
+
 from db.crud import get_or_create_user
 
 from config import load_environment_variables
@@ -17,12 +18,68 @@ bot = Bot(token=BOT_API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     user_id = message.from_user.id
     user = get_or_create_user(user_id)
-    await message.answer(str(user.id))
+    if user.refresh_token is None:
+        await message.answer('Привет! Чтобы получить статистику, Вам необходимо авторизоваться через Spotify.')
+    else: 
+        await message.answer('Список команд можно увидеть, написав /help.')      
+
+  
+@dp.message_handler(commands=['help'])
+async def help(message: types.Message):
+    await message.reply("Доступные команды: /stats, /auth")
+
+
+@dp.message_handler(commands=['auth'])
+async def auth(message: types.Message):
+    pass
+
+
+@dp.inline_handler()
+async def inline_handler(query: types.InlineQuery):
+    user_id = query.from_user.id
+    user = get_or_create_user(user_id)
+    if user.refresh_token is None:
+        results = []
+        results.append(
+            types.InlineQueryResultArticle(
+                id='1',
+                title='Моя статистика',
+                description='Чтобы получить статистику, Вам необходимо авторизоваться в боте через Spotify.',
+                input_message_content=types.InputTextMessageContent(
+                    message_text='Чтобы получить статистику, Вам необходимо авторизоваться в боте через Spotify.',                  
+                ),
+                reply_markup=types.InlineKeyboardMarkup().add(
+                    types.InlineKeyboardButton(
+                        text='Авторизоваться',
+                        callback_data='auth',
+                    ) # type: ignore
+                )
+            )
+        )
+        await query.answer(results, cache_time=1, is_personal=True)
+    else:
+        results = []
+        results.append(
+            types.InlineQueryResultArticle(
+                id='1',
+                title='Моя статистика',
+                description='Посмотреть топ моих самых прослушиваемых треков',
+                input_message_content=types.InputTextMessageContent(
+                    message_text=''
+                ),
+            )
+        )
+        await query.answer(results, cache_time=1, is_personal=True)
+
 
 if __name__ == '__main__':
     from aiogram import executor
-    executor.start_polling(dp, skip_updates=True)
+    from services import spotify_auth
+    
+    spotify_auth.main()
+    # executor.start_polling(dp, skip_updates=True)
---
+++ services/spotify_auth.py
@@ -0,0 +1,75 @@
+import os
+import urllib.parse
+import requests
+from db.crud import update_user, get_user
+
+from config import load_environment_variables
+load_environment_variables()
+
+
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+redirect_uri = os.getenv("REDIRECT_URI")
+scope = 'user-read-private user-top-read'
+token_url = 'https://accounts.spotify.com/api/token'
+auth_url = 'https://accounts.spotify.com/authorize'
+
+
+def generate_auth_link():
+    params = {
+        'response_type': 'code',
+        'client_id': client_id,
+        'redirect_uri': redirect_uri,
+        'scope': scope
+    }
+
+    return f"{auth_url}?{urllib.parse.urlencode(params)}"
+
+def get_access_token(user_id, code):
+
+    payload = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': redirect_uri,
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
+
+    response = requests.post(token_url, data=payload)
+
+    if response.status_code == 200:
+        token_info = response.json()
+        access_token = token_info.get("access_token")
+        refresh_token = token_info.get("refresh_token")
+
+        update_user(user_id, access_token, refresh_token)
+        return 'Вы успешно прошли авторизацию'
+    else:
+        print(response.json())
+        return "Произошла ошибка"
+
+def refresh_access_token(user_id):
+    user = get_user(user_id)
+    if not user or user.refresh_token is None:
+        return "Ошибка авторизации. Refresh token не найден"
+    
+    refresh_token = user.refresh_token
+
+    payload = {
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token,
+        "client_id": client_id,
+        "client_secret": client_secret
+    }
+
+    response = requests.post(token_url, data=payload)
+
+    if response.status_code == 200:
+        token_info = response.json()
+        new_access_token = token_info.get("access_token")
+
+        update_user(user_id, new_access_token)
+    else:
+        print("Refresh token error: ", response.json())
+    
+    return response.status_code
