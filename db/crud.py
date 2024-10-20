2e40d33e26e38cd3604088788ec08e3ef220d0d7
Nnley <kragelek86@gmail.com>
2024-10-21 00:50:39 +0300

    Refactor functions to classes following SOLID principles

--- db/crud.py
+++ db/crud.py
@@ -1,29 +1,109 @@
 from db.database import Session
 from db.models import User
 
-session = Session()
+class UserRepository:
+    def get_user_by_id(self, user_id):
+        with Session() as session:
+            return session.query(User).filter_by(id=user_id).first()
 
-def get_or_create_user(user_id):
-    user = session.query(User).filter_by(id=user_id).first()
+    def add_user(self, user):
+        with Session() as session:
+            session.add(user)
+            session.commit()
+
+    def update_user(self, user):
+        with Session() as session:
+            session.merge(user)
+            session.commit()
+
+
+class UserManager:
+    def __init__(self, user_repository):
+        self.user_repository = user_repository
+
+    def get_or_create_user(self, user_id):
+        user = self.user_repository.get_user_by_id(user_id)
+        
+        if user is None:
+            user = User(id=user_id)
+            self.user_repository.add_user(user)
+        
+        return user
+
+
+class UserTokenManager: 
+    def __init__(self, user_repository):
+        self.user_repository = user_repository
     
-    if user is None:
-        user = User(id=user_id)
-        session.add(user)
-        session.commit()
+    def get_access_token(self, user_id):
+        user = self.user_repository.get_user_by_id(user_id)
+        if user:
+            return user.access_token
+        return None
+
+    def get_refresh_token(self, user_id):
+        user = self.user_repository.get_user_by_id(user_id)
+        if user:
+            return user.refresh_token
+        return None
     
-    return user
-
-def get_user(user_id):
-    return session.query(User).filter_by(id=user_id).first()
-
-def update_user(user_id, access_token=None, refresh_token=None, top_tracks=None):
-    user = get_user(user_id)
-    if user:
-        if access_token is not None:
+    def set_access_token(self, user_id, access_token):
+        user = self.user_repository.get_user_by_id(user_id)
+        if user:
             user.access_token = access_token
-        if refresh_token is not None:
+            self.user_repository.update_user(user)
+        return user
+    
+    def set_refresh_token(self, user_id, refresh_token):
+        user = self.user_repository.get_user_by_id(user_id)
+        if user:
             user.refresh_token = refresh_token
-        if top_tracks is not None:
-            user.top_tracks = top_tracks
-        session.commit()
-    return user
+            self.user_repository.update_user(user)
+        return user
+    
+
+class UserTrackManager:
+    def __init__(self, user_repository):
+        self.user_repository = user_repository
+    
+    def get_top_tracks_month(self, user_id):
+        user = self.user_repository.get_user_by_id(user_id)
+        if user:
+            return user.top_tracks_month
+        else:
+            return None
+    
+    def get_top_tracks_half_year(self, user_id):
+        user = self.user_repository.get_user_by_id(user_id)
+        if user:
+            return user.top_tracks_half_year
+        else:
+            return None
+    
+    def get_top_tracks_year(self, user_id):
+        user = self.user_repository.get_user_by_id(user_id)
+        if user:
+            return user.top_tracks_year
+        else:
+            return None
+    
+    def set_top_tracks_month(self, user_id, top_tracks_month):
+        user = self.user_repository.get_user_by_id(user_id)
+        if user:
+            user.top_tracks_month = top_tracks_month
+            self.user_repository.update_user(user)
+        return user
+    
+    def set_top_tracks_half_year(self, user_id, top_tracks_half_year):
+        user = self.user_repository.get_user_by_id(user_id)
+        if user:
+            user.top_tracks_half_year = top_tracks_half_year
+            self.user_repository.update_user(user)
+        return user
+
+    def set_top_tracks_year(self, user_id, top_tracks_year):
+        user = self.user_repository.get_user_by_id(user_id)
+        if user:
+            user.top_tracks_year = top_tracks_year
+            self.user_repository.update_user(user)
+        return user
