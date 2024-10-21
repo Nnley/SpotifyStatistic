4757d7778e7adfa6b9fdc3e017323f2ec9e1d122
Nnley <kragelek86@gmail.com>
2024-10-21 06:41:50 +0300

    Refactor functions to classes following SOLID principles

--- services/spotify_service.py
+++ services/spotify_service.py
@@ -1,11 +1,11 @@
 import requests
 import enum
 
-from db.crud import UserManager, UserRepository, UserTokenManager, UserTrackManager, UserNotFoundError
-from db.types import TopTracksType
+from db.crud import UserManager, UserRepository, UserTokenManager, UserTrackManager
+from db.types import TopTracksType, IUser
 
 from services.spotify_auth import SpotifyAuth, NotAuthorizedError
-from typing import Optional, List
+from typing import Optional, List, cast
 
 
 class TimeRange(enum.Enum):
@@ -14,72 +14,71 @@ class TimeRange(enum.Enum):
     long_term = 'long_term'
 
 
-class SpotifyService():
-    def __init__(self) -> None:
-        pass
+class SpotifyAPI:
+    def __init__(self):
+        self.base_url = 'https://api.spotify.com/v1'
     
-    @staticmethod
-    def get_user_top_tracks(user_id: int, time_range: TimeRange) -> Optional[List[TopTracksType]]:
-        instance = SpotifyService()
+    def fetch_top_tracks(self, access_token: str, time_range: TimeRange) -> tuple[int, dict]:
+        url = f'{self.base_url}/me/top/tracks?limit=10&time_range={time_range}'
+        response = requests.get(url, headers={'Authorization': f'Bearer {access_token}'})
+        return response.status_code, response.json()
+
+    def fetch_user_profile(self, access_token: str) -> tuple[int, dict]:
+        url = f'{self.base_url}/me'
+        response = requests.get(url, headers={'Authorization': f'Bearer {access_token}'})
+        return response.status_code, response.json()
+
+
+class SpotifyService():
+    def __init__(self):
+        self.spotify_api = SpotifyAPI()
+
+    def get_user_top_tracks(self, user_id: int, time_range: TimeRange) -> Optional[List[TopTracksType]]:
         user = UserManager.get_or_create_user(user_id)
 
         if user.access_token is None or user.refresh_token is None:
-            raise NotAuthorizedError("User is not authorized")
+            raise NotAuthorizedError(f"User {user.id} is not authorized")
 
-        status_code, data = instance._request_top_tracks(user.access_token, time_range)
+        status_code, data = self.spotify_api.fetch_top_tracks(user.access_token, time_range)
         
         if status_code == 401:
-            try: 
-                spotify_auth = SpotifyAuth()
-                user.access_token = spotify_auth.refresh_access_token(user.refresh_token)
-                user = update_user(user.id, user.access_token)
-                status_code, data = SpotifyService._request_top_tracks(user.access_token, time_range)
-            except Exception as e:
-                print(e)
-                return
-        
+            user = self.refresh_user_tokens(user)
+            status_code, data = self.spotify_api.fetch_top_tracks(cast(str, user.access_token), time_range)
+
         if status_code != 200:
-            print(data, status_code)
-            return
-        
-        result = []
-        for track in data.get('items'):
-            result.append({'name': track.get('name'), 'artist': track.get('artists')[0].get('name') })
-        
-        return result
+            raise Exception(f'Request top tracks error with status code: {status_code}')
 
-    def _request_top_tracks(self, access_token, time_range):
-        response = requests.get(f'https://api.spotify.com/v1/me/top/tracks?limit=10&time_range={time_range}', headers={
-                    'Authorization': f'Bearer {access_token}'
-                })
-        return response.status_code, response.json()
+        return self.parse_top_tracks(data)
 
-    def _request_get_user_profile(self, access_token):
-        response = requests.get('https://api.spotify.com/v1/me', headers={
-                'Authorization': f'Bearer {access_token}'  # type: ignore
-                })
-        return response.status_code, response.json()
+    def get_user_profile(self, user_id):
+        user = UserManager.get_or_create_user(user_id)
 
-    @staticmethod
-    def get_user_profile(user_id):
-        user = get_user(user_id)
+        if user.access_token is None or user.refresh_token is None:
+            raise NotAuthorizedError(f"User {user.id} is not authorized")
         
-        if user and user.access_token is None:
-            return
-        
-        status_code, data = request_get_user_profile(user.access_token) # type: ignore
+        status_code, data = self.spotify_api.fetch_user_profile(user.access_token)
         
         if status_code == 401:
-            try: 
-                user.access_token = refresh_access_token(user.id) # type: ignore
-                user = update_user(user.id, user.access_token) # type: ignore
-                status_code, data = request_get_user_profile(user.access_token) # type: ignore
-            except Exception as e:
-                print(e)
-                return
+            user = self.refresh_user_tokens(user)
+            status_code, data = self.spotify_api.fetch_user_profile(cast(str, user.access_token))
         
         if status_code != 200:
-            print(data, status_code)
-            return
+            raise Exception(f'Request user profile error with status code: {status_code}')
         
         return data
+    
+    def refresh_user_tokens(self, user: IUser) -> IUser:
+        try:    
+            spotify_auth = SpotifyAuth()
+            user.refresh_token, user.access_token = spotify_auth.refresh_access_token(cast(str, user.refresh_token))
+            UserTokenManager.set_access_token(user.id, user.access_token)
+            UserTokenManager.set_refresh_token(user.id, user.refresh_token)
+            return user
+        except Exception as e:
+            raise Exception(f"Failed to refresh access token: {e}")
+
+    def parse_top_tracks(self, data):
+        result = []
+        for track in data.get('items', []):
+            result.append({'name': track.get('name'), 'artist': track.get('artists')[0].get('name')})
+        return result
