b5b31eda694f06ef9f237c997749bf0b809b7ea8
Nnley <kragelek86@gmail.com>
2024-10-22 10:40:46 +0300

    Added auth_callback handler

--- handlers/user/auth.py
+++ handlers/user/auth.py
@@ -3,6 +3,7 @@ import os
 from aiogram import Dispatcher
 from aiogram import types
 
+from loader import bot 
 from db.crud import UserManager
 from localization import get_text
 from keyboards.reply import auth_reply_keyboard
@@ -22,5 +23,15 @@ async def auth(message: types.Message):
     else:
         await message.answer(get_text(user.language_code, 'already_authorized'))
     
+async def auth_callback(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    
+    user_id = callback_query.from_user.id
+    user_language = callback_query.from_user.language_code
+    user = UserManager.get_or_create_user(user_id, user_language)
+    
+    await bot.send_message(user_id, get_text(user.language_code, 'none_auth_start'))
+
 def register_auth(dp: Dispatcher):
     dp.register_message_handler(auth, commands=["auth"], state="*")
+    dp.register_callback_query_handler(auth_callback, lambda c: c.data == 'auth-callback')
