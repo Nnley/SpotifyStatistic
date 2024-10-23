dee160f099dd3d91379c067d6d079488ae8ae9f9
Nnley <kragelek86@gmail.com>
2024-10-23 04:03:08 +0300

    Edited generation of auth link

--- handlers/user/auth.py
+++ handlers/user/auth.py
@@ -1,9 +1,10 @@
 import os
+import uuid
 
 from aiogram import Dispatcher
 from aiogram import types
 
-from db.crud import UserManager
+from db.crud import UserManager, AuthorizationCodeManager
 from localization import get_text
 from keyboards.reply import auth_reply_keyboard
 
@@ -14,9 +15,12 @@ async def auth(message: types.Message):
     user_id = message.from_user.id
     user_language = message.from_user.language_code
     user = UserManager.get_or_create_user(user_id, user_language)
+    code = str(uuid.uuid4())
+    
+    AuthorizationCodeManager.set_authorization_code(user_id, code)
     
     if user.refresh_token is None:
-        auth_link = f'{os.getenv("SPOTIFY_AUTH_URL")}/{user_id}'
+        auth_link = f'{os.getenv("SPOTIFY_AUTH_URL")}/{code}'
         
         await message.answer(get_text(user.language_code, 'auth_button_description'), reply_markup=auth_reply_keyboard(auth_link))
     else:
