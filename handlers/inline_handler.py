4b4c29e960592de1bbcc151d505231d3317bb26f
Nnley <kragelek86@gmail.com>
2024-10-23 03:30:08 +0300

    Added user top artists

--- handlers/inline_handler.py
+++ handlers/inline_handler.py
@@ -1,8 +1,10 @@
 from services.spotify_service import SpotifyService, TimeRange
 from aiogram import types
 from db.crud import UserManager
+from db.types import TopTracksType, TopArtistsType, IUserProfile
 from aiogram import Dispatcher
 from localization import get_text
+from typing import List, Optional
 
 async def create_inline_result(id: str, title: str, description: str, message_text: str, reply_markup=None) -> types.InlineQueryResultArticle:
     return types.InlineQueryResultArticle(
@@ -15,9 +17,9 @@ async def create_inline_result(id: str, title: str, description: str, message_te
         reply_markup=reply_markup
     )
 
-async def get_user_top_tracks_message(user_profile, user_top_tracks, time_period: str, language_code: str) -> str:
-    if user_top_tracks is not None and user_profile is not None:
-        message_template = get_text(language_code, 'inline_query_message')
+async def get_user_top_tracks_message(user_profile: IUserProfile, user_top_tracks: Optional[List[TopTracksType]], time_period: str, language_code: str) -> str:
+    if user_top_tracks is not None:
+        message_template = get_text(language_code, 'tracks_inline_query_message')
         message_text = message_template.format(
             display_name=user_profile.get('display_name'),
             country=user_profile.get('country'),
@@ -25,8 +27,18 @@ async def get_user_top_tracks_message(user_profile, user_top_tracks, time_period
         )
         
         message_text += '\n\n' + '\n'.join([f"{i + 1}. {track.get('name')} - {track.get('artist')}" for i, track in enumerate(user_top_tracks)])
-    else:
-        message_text = get_text(language_code, 'error_message')
+    return message_text
+
+async def get_user_top_artists_message(user_profile: IUserProfile, user_top_artists: Optional[List[TopArtistsType]], time_period: str, language_code: str) -> str:
+    if user_top_artists is not None:
+        message_template = get_text(language_code, 'artists_inline_query_message')
+        message_text = message_template.format(
+            display_name=user_profile.get('display_name'),
+            country=user_profile.get('country'),
+            time_period=time_period
+        )
+        
+        message_text += '\n\n' + '\n'.join([f"{i + 1}. {track.get('name')}" for i, track in enumerate(user_top_artists)])
     return message_text
 
 async def inline_handler(query: types.InlineQuery):
@@ -39,7 +51,7 @@ async def inline_handler(query: types.InlineQuery):
         results.append(
             await create_inline_result(
                 id='1',
-                title=get_text(user.language_code, 'inline_query_title'),
+                title=get_text(user.language_code, 'none_auth_inline_query_title'),
                 description=get_text(user.language_code, 'none_auth_get_stats'),
                 message_text=get_text(user.language_code, 'none_auth_get_stats'),
                 reply_markup=types.InlineKeyboardMarkup().add(
@@ -52,39 +64,75 @@ async def inline_handler(query: types.InlineQuery):
         )
     else:
         spotify_service = SpotifyService()
+        
         user_top_tracks_month = spotify_service.get_user_top_tracks(user_id, TimeRange.SHORT_TERM)
         user_top_tracks_half_year = spotify_service.get_user_top_tracks(user_id, TimeRange.MEDIUM_TERM)
         user_top_tracks_year = spotify_service.get_user_top_tracks(user_id, TimeRange.LONG_TERM)
+        
+        user_top_artists_month = spotify_service.get_user_top_artists(user_id, TimeRange.SHORT_TERM)
+        user_top_artists_half_year = spotify_service.get_user_top_artists(user_id, TimeRange.MEDIUM_TERM)
+        user_top_artists_year = spotify_service.get_user_top_artists(user_id, TimeRange.LONG_TERM)
+        
         user_profile = spotify_service.get_user_profile(user_id)
 
         top_tracks_month_message_text = await get_user_top_tracks_message(user_profile, user_top_tracks_month, get_text(user.language_code, 'word_month'), user.language_code)
         top_tracks_half_year_message_text = await get_user_top_tracks_message(user_profile, user_top_tracks_half_year, get_text(user.language_code, 'word_half_year'), user.language_code)
         top_tracks_year_message_text = await get_user_top_tracks_message(user_profile, user_top_tracks_year, get_text(user.language_code, 'word_year'), user.language_code)
 
-        description_template = get_text(user.language_code, 'inline_query_description')
+        top_artists_month_message_text = await get_user_top_artists_message(user_profile, user_top_artists_month, get_text(user.language_code, 'word_month'), user.language_code)
+        top_artists_half_year_message_text = await get_user_top_artists_message(user_profile, user_top_artists_half_year, get_text(user.language_code, 'word_half_year'), user.language_code)
+        top_artists_year_message_text = await get_user_top_artists_message(user_profile, user_top_artists_year, get_text(user.language_code, 'word_year'), user.language_code)
+
+        tracks_description_template = get_text(user.language_code, 'tracks_inline_query_description')
+        artists_description_template = get_text(user.language_code, 'artists_inline_query_description')
 
         results.append(
             await create_inline_result(
                 id='2',
-                title=get_text(user.language_code, 'word_statistic').title(),
-                description=description_template.format(time_period=get_text(user.language_code, 'word_month')),
+                title=get_text(user.language_code, 'tracks_inline_query_title'),
+                description=tracks_description_template.format(time_period=get_text(user.language_code, 'word_month')),
                 message_text=top_tracks_month_message_text,
             )
         )
         results.append(
             await create_inline_result(
                 id='3',
-                title=get_text(user.language_code, 'word_statistic').title(),
-                description=description_template.format(time_period=get_text(user.language_code, 'word_half_year')),
+                title=get_text(user.language_code, 'tracks_inline_query_title'),
+                description=tracks_description_template.format(time_period=get_text(user.language_code, 'word_half_year')),
                 message_text=top_tracks_half_year_message_text,
             )
         )
         results.append(
             await create_inline_result(
                 id='4',
-                title=get_text(user.language_code, 'word_statistic').title(),
-                description=description_template.format(time_period=get_text(user.language_code, 'word_year')),
+                title=get_text(user.language_code, 'tracks_inline_query_title'),
+                description=tracks_description_template.format(time_period=get_text(user.language_code, 'word_year')),
                 message_text=top_tracks_year_message_text,
+            )
+        )
+        
+        results.append(
+            await create_inline_result(
+                id='5',
+                title=get_text(user.language_code, 'artists_inline_query_title'),
+                description=artists_description_template.format(time_period=get_text(user.language_code, 'word_month')),
+                message_text=top_artists_month_message_text,
+            )
+        )
+        results.append(
+            await create_inline_result(
+                id='6',
+                title=get_text(user.language_code, 'artists_inline_query_title'),
+                description=artists_description_template.format(time_period=get_text(user.language_code, 'word_half_year')),
+                message_text=top_artists_half_year_message_text,
+            )
+        )
+        results.append(
+            await create_inline_result(
+                id='7',
+                title=get_text(user.language_code, 'artists_inline_query_title'),
+                description=artists_description_template.format(time_period=get_text(user.language_code, 'word_year')),
+                message_text=top_artists_year_message_text,
             )
         )
 
